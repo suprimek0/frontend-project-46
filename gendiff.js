867fde04c7f35bab41743f2d7327bd7231f52b77
Suprimek <Suprimeko0@gmail.com>
2026-04-20 13:48:12 +0300

    feat: добавлены аргументы filepath и опция --format  в CLI утилиты gendiff

--- gendiff.js
+++ gendiff.js
@@ -6,5 +6,17 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
+
+/* Временный обработчик для аргументов и опций. Проверка работоспособности утилиты на принятие двух аргументов.
+program.action((filepath1, filepath2) => {
+  const options = program.opts();
+  console.log('File 1:', filepath1);
+  console.log('File 2:', filepath2);
+  console.log('Format:', options.format || 'default');
+});
+*/
+
 
 program.parse();
