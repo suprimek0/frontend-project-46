9a17b31ab36f633bf23d577e65f7c48095140457
Suprimek <Suprimeko0@gmail.com>
2026-04-20 14:31:43 +0300

    chore: добавлен ESLint в проект с базовой конфигурацией

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
