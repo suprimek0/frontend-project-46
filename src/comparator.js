761dff30553db37f5edc98467c25c58c7f8c1ff1
Suprimek <Suprimeko0@gmail.com>
2026-04-27 19:01:09 +0300

    feat:реализована поддержка сравнения вложенных структур (JSON/YAML)

--- src/comparator.js
+++ src/comparator.js
@@ -1,23 +1,41 @@
 import _ from 'lodash';
 
-const compare = (data1, data2) => {
+const isObject = (value) => typeof value === 'object' && value !== null && !Array.isArray(value);
+
+const compare = (data1 = {}, data2 = {}) => {
   const allKeys = _.sortBy([...new Set([...Object.keys(data1), ...Object.keys(data2)])]);
 
   return allKeys.map((key) => {
     if (!(key in data1)) {
       return { key, type: 'added', value: data2[key] };
     }
+
     if (!(key in data2)) {
       return { key, type: 'removed', value: data1[key] };
     }
-    if (data1[key] !== data2[key]) {
-      return [
-        { key, type: 'removed', value: data1[key] },
-        { key, type: 'added', value: data2[key] }
-      ];
+
+    const value1 = data1[key];
+    const value2 = data2[key];
+
+    if (isObject(value1) && isObject(value2)) {
+      return {
+        key,
+        type: 'nested',
+        children: compare(value1, value2)
+      };
     }
-    return { key, type: 'unchanged', value: data1[key] };
-  }).flat();
+
+    if (value1 !== value2) {
+      return {
+        key,
+        type: 'changed',
+        oldValue: value1,
+        newValue: value2
+      };
+    }
+
+    return { key, type: 'unchanged', value: value1 };
+  });
 };
 
 export default compare;
