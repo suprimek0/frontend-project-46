2f776cb7f9bc69d52518c0fcbf747d5317b8b27d
Suprimek <Suprimeko0@gmail.com>
2026-04-27 23:10:42 +0300

    ref: добавлен json в фабрику форматтеров

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,13 @@
 
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js'
 
 const getFormatter = (formatName = 'stylish') => {
   const formatters = {
     stylish,
     plain,
+    json,
   };
 
   if (!formatters[formatName]) {
