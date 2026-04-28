e699506663df99e99da5599c9d192a50c20aec2e
Suprimek <Suprimeko0@gmail.com>
2026-04-28 14:45:36 +0300

    feat: обновлена интеграция с SonarCloud

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,19 @@ export default {
   },
   testEnvironmentOptions: {
     url: 'http://localhost'
-  }
-}
+  },
+  // Настройки покрытия
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'json', 'text'],
+  // Генерация XML для SonarQube
+  reporters: [
+    'default',
+    ['jest-sonar-reporter', {
+      outputFile: 'coverage/test-results.xml',
+      coverageFolder: 'coverage',
+      reportPath: 'coverage',
+      reportFile: 'test-results.xml'
+    }]
+  ]
+};
